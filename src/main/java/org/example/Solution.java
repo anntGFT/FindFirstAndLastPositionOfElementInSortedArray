a6d493e45680da493fdcae1ae5c8f47f3eafdfe7
annt <annt@gft.com>
2022-11-28 12:23:22 +0000

    Done with boolean and for loop

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,36 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] result;
+        int startingPosition = -1;
+        int endingPosition = -1;
+        boolean aux = true;
+
+
+        for(int i = 0; i< nums.length;i++){
+
+            if (nums[i] == target && aux){
+
+                startingPosition = i;
+
+                aux = false;
+            }
+
+            if ( nums[i] == target){
+
+                endingPosition = i;
+            }
+
+        }
+
+        result = new int[] {startingPosition, endingPosition};
+
+        return result;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,78 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int[] result, array;
+    int target;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void givenEmptyArray(){
+
+        target = 0;
+        result = new int[] {-1,-1};
+        array = new int[] {};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.searchRange(array, target)));
+
+    }
+
+    @Test
+    public void targetNotMatchingAnyNumberOfTheArray(){
+
+        target = 0;
+        result = new int[] {-1,-1};
+        array = new int[] {1,2,3,4,5,6,7,8,9};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.searchRange(array, target)));
+
+    }
+
+    @Test
+    public void targetMatchingOnlyOneNumberOfTheArray(){
+
+        target = 3;
+        result = new int[] {2,2};
+        array = new int[] {1,2,3,4,5,6,7,8,9};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.searchRange(array, target)));
+
+    }
+
+    @Test
+    public void targetMatchingTwoNumbersOfTheArray(){
+
+        target = 3;
+        result = new int[] {2,6};
+        array = new int[] {1,2,3,4,5,6,3,8,9};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.searchRange(array, target)));
+
+    }
+
+    @Test
+    public void targetMatchingFourNumbersOfTheArray(){
+
+        target = 5;
+        result = new int[] {0,8};
+        array = new int[] {5,3,3,4,5,5,3,8,5,9};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.searchRange(array, target)));
+
+    }
 }
+
